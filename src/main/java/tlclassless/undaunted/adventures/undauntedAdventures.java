cb9e52464c9c07eaba0933a2e6b3eaf40bdd8be1
Ben <classle55sc2@gmail.com>
2021-01-25 22:50:22 +0000

    registered first item, yeti tooth.

--- src/main/java/tlclassless/undaunted/adventures/undauntedAdventures.java
+++ src/main/java/tlclassless/undaunted/adventures/undauntedAdventures.java
@@ -1,14 +1,20 @@
 package tlclassless.undaunted.adventures;
 
+import net.minecraft.util.registry.*;
+
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
 
-public class undauntedAdventures implements ModInitializer {
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
-	}
+public class undauntedAdventures implements ModInitializer
+{
+    // an instance of our new item
+    public static final Item YETI_TOOTH = new Item(new Item.Settings().group(ItemGroup.MISC));
+ 
+    @Override
+    public void onInitialize()
+    {
+        Registry.register(Registry.ITEM, new Identifier("undauntedadventures", "yeti_tooth"), YETI_TOOTH);
+	} 
 }
