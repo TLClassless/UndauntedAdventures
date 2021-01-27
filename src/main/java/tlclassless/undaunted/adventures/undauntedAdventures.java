620901818d99f91775458eae1693e4d710411a81
Ben <classle55sc2@gmail.com>
2021-01-27 16:18:39 +0000

    added and textured two component items

--- src/main/java/tlclassless/undaunted/adventures/undauntedAdventures.java
+++ src/main/java/tlclassless/undaunted/adventures/undauntedAdventures.java
@@ -1,20 +1,23 @@
 package tlclassless.undaunted.adventures;
 
 import net.minecraft.util.registry.*;
-
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 
-public class undauntedAdventures implements ModInitializer
+public class UndauntedAdventures implements ModInitializer
 {
     // an instance of our new item
     public static final Item YETI_TOOTH = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item DRAGON_EYE = new Item(new Item.Settings().group(ItemGroup.MISC));
  
     @Override
     public void onInitialize()
     {
         Registry.register(Registry.ITEM, new Identifier("undauntedadventures", "yeti_tooth"), YETI_TOOTH);
-	} 
+    }
+    {
+        Registry.register(Registry.ITEM, new Identifier("undauntedadventures", "dragon_eye"), DRAGON_EYE);
+	}  
 }
