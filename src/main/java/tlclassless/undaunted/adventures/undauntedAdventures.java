b811b675e75a78ca5395f11d04c10d5f2dd874a4
Ben <classle55sc2@gmail.com>
2021-01-28 23:31:26 +0000

    added multiple items and recipe

--- src/main/java/tlclassless/undaunted/adventures/undauntedAdventures.java
+++ src/main/java/tlclassless/undaunted/adventures/undauntedAdventures.java
@@ -11,6 +11,7 @@ public class UndauntedAdventures implements ModInitializer
     // an instance of our new item
     public static final Item YETI_TOOTH = new Item(new Item.Settings().group(ItemGroup.MISC));
     public static final Item DRAGON_EYE = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item YETI_TOOTH_NECKLACE = new Item(new Item.Settings().group(ItemGroup.MISC));
  
     @Override
     public void onInitialize()
@@ -19,5 +20,8 @@ public class UndauntedAdventures implements ModInitializer
     }
     {
         Registry.register(Registry.ITEM, new Identifier("undauntedadventures", "dragon_eye"), DRAGON_EYE);
-	}  
+    }  
+    {
+        Registry.register(Registry.ITEM, new Identifier("undauntedadventures", "yeti_tooth_necklace"), YETI_TOOTH_NECKLACE);
+    }
 }
